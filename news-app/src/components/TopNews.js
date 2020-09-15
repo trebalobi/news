a96bd6c26efcc1525714e14d1aea194376349b31
trebalobi <mimilica.dj96@gmail.com>
2020-09-15 12:11:07 +0200

    Added basic news preview component

--- news-app/src/components/TopNews.js
+++ news-app/src/components/TopNews.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import NewsPreview from './NewsPreview';
 
 export default class TopNews extends Component {
   render() {
-    return <div>top news</div>;
+    return (
+      <div className="top-news">
+        <NewsPreview id={1}></NewsPreview>
+        <NewsPreview id={2}></NewsPreview>
+        <NewsPreview id={3}></NewsPreview>
+        <NewsPreview id={4}></NewsPreview>
+      </div>
+    );
   }
 }
